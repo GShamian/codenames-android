d870a0d9d50afca73911ae6d95160ec74a4860c6
George Shamian <gosha.shamian@yandex.ru>
2020-04-25 21:06:44 +0300

    cleanup code and added locations @GShamian

--- Backend/client.py
+++ Backend/client.py
@@ -7,29 +7,35 @@ def createLobby(players_amount, spy_amount):
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.connect(address_to_server)
     client.send(bytes("CREATE_LOBBY {players_amount} {spy_amount}".format(players_amount = players_amount, spy_amount = spy_amount), encoding='UTF-8'))
+    # Декодирую объект
     data = pickle.loads(client.recv(1024))
-    return (data[0], data[1], data[2:]) 
+    # data[0] - токен
+    # data[1] - роль
+    # data[2] - загаданная локация
+    # data[3] - массив локаций
+    return (data[0], data[1], data[2], data[3]) 
 
 def connect(token):
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.connect(address_to_server)
     client.send(bytes(token, encoding='UTF-8'))
+    # Декодирую объект
     data = pickle.loads(client.recv(1024))
-    return (data[0], data[1])
+    # data[0] - роль
+    # data[1] - загаданная локация
+    # data[2] - массив локаций
+    return (data[0], data[1], data[2])
 
-def checkLocation(role, location):
-    if role == 'spy':
-        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        client.connect(address_to_server)
-        client.send(bytes(location, encoding='UTF-8'))
-        # Получаем ответ от сервера является ли эта локация верной
-        response = client.recv(1024).decode()
-        return response
-    elif role == 'peaceful':
-        return 'invalid role'
+def checkLocation(token, location):
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect(address_to_server)
+    client.send(bytes(str(location) + str(token), encoding='UTF-8'))
+    # Получаем ответ от сервера является ли эта локация верной
+    response = client.recv(1024).decode()
+    return response
 
 
 
 token = createLobby(5,2)
-print(token)
+print(token[0])
 print(connect(token[0]))
--- Backend/server.py
+++ Backend/server.py
@@ -17,6 +17,8 @@ print('server is running, please, press ctrl+c to stop')
 spy_amount = []
 players_amount = []
 tokens = []
+key_locations = []
+
 database = ['Bank', 'Hospital', 'Military unit', 'Casino',
             'Hollywood', 'Titanic', 'The Death Star', 'Hotel',
             'Russian Railways', 'Malibu Beach', 'Police Station',
@@ -27,58 +29,68 @@ while True:
 
     connection, address = server_socket.accept()
     data = connection.recv(1024).decode().split()
-    #print(data)
 
     # Выдача токена пользователю
     if 'CREATE_LOBBY' in data:
-        
+
+        # Генерируем токен и загадываем локацию
         token = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in range(4))
-        tokens.append(token)
-        print(tokens)
+        key_location = database[random.randint(0,15)]
+        token_role_keyLocation_locations = []
         
+        # Добавляем токен, загаданную локацию, число шпионов и игроков в соответствующие массивы
+        tokens.append(token)
+        key_locations.append(key_location)
         spy_amount.append(int(data[1]))
         players_amount.append(int(data[2]))
-        
-        token_role_locations = []
-        token_role_locations.append(token)
+        token_role_keyLocation_locations.append(token)
 
+        # Генерируем роль
         role = random.randint(0,1)
         if role == 1:
-            token_role_locations.append('spy')
+            token_role_keyLocation_locations.append('spy')
             spy_amount[-1] -= 1
         elif role == 0:
-            token_role_locations.append('peaceful')
+            token_role_keyLocation_locations.append('peaceful')
             players_amount[-1] -= 1
 
-        
-        token_role_locations.append(database)
-        #connection.send(bytes(token, encoding='UTF-8'))
-        connection.send(pickle.dumps(token_role_locations))
+        # Создаем посылку с токеном, ролью, загаданными и оставшимися локациями
+        token_role_keyLocation_locations.append(key_location)
+        token_role_keyLocation_locations.append(database)
+        # Сериализуем объект и отправляем его клиенту
+        connection.send(pickle.dumps(token_role_keyLocation_locations))
         
     
     # Подключение мазефакера к лобби и выдача ему роли
-    elif "".join(data) in tokens: 
-        data = "".join(data)
+    elif ''.join(data) in tokens: 
+        data = ''.join(data)
         # Находим конкретное лобби по указанному токену
         spy_amount_current = spy_amount[tokens.index(data)]
         players_amount_current = players_amount[tokens.index(data)]
+        key_location_current = key_locations[tokens.index(data)]
 
-        # Определяю роли
+        # Генерируем роль мазефакеру и добавляю его в посылку
         role = random.randint(0,1)
+        role_keyLocation_locations = []
         if role == 1 and spy_amount_current != 0:
-            role_locations = []
-            role_locations.append('spy')
-            role_locations.append(database)
-            print(role_locations)
-            connection.send(pickle.dumps(role_locations))
+            role_keyLocation_locations.append('spy')
             spy_amount_current -= 1
         elif role == 0 and players_amount != 0:
-            role_locations = []
-            role_locations.append('peaceful')
-            role_locations.append(database)
-            print(role_locations)
-            connection.send(pickle.dumps(role_locations))
+            role_keyLocation_locations.append('peaceful')
             players_amount_current -= 1
-            
+        
+        # Добавляем загаданную и оставшиеся локации в посылку
+        role_keyLocation_locations.append(key_location_current)
+        role_keyLocation_locations.append(database)
+        # Отправляем посылку клиенту в двоичном формате
+        connection.send(pickle.dumps(role_keyLocation_locations))
+
+    elif ''.join(data[0]) in key_locations:
+        if data[1] in tokens:
+            if tokens.index(data[1]) == key_locations.index(data[0]):
+                connection.send(bytes('true', encoding='UTF-8'))
+        else:
+            connection.send(bytes('false', encoding='UTF-8'))
+
     # Закрываем сокет (бб мазафакер)
     connection.close()
