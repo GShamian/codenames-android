01093a227b49cc3b37ab3915e8f5ca45589ece45
deftshadow <58772248+deftshadow@users.noreply.github.com>
2020-04-25 18:37:01 +0300

    new screen + updated graphics @deftshadow

--- Graphics/main.py
+++ Graphics/main.py
@@ -1,12 +1,21 @@
 import kivy 
+import client
+from kivy import *
+from io import open
 from kivy.app import App 
-from kivy.lang import builder
+from kivy.uix.button import Button
+from kivy.uix.gridlayout import GridLayout 
+from kivy.uix.label import Label 
+from kivy.lang import Builder
+from kivy.uix.popup import Popup
 from kivy.clock import Clock
-from kivy.properties import ObjectProperty, \
-    NumericProperty,\
-    StringProperty
+#from kivy.properties import ObjectProperty, NumericProperty, StringProperty
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.textinput import TextInput
+from kivymd.toast.kivytoast import toast
+from kivymd.theming import ThemeManager
+from kivymd.app import MDApp
+
 
 
 class FirstScreen(Screen):
@@ -17,7 +26,10 @@ class SecondScreen(Screen):
     pass
 
 class ThirdScreen(Screen):
-    spy_amount = NumericProperty(0)
+
+    spy_amount = kivy.properties.NumericProperty(0)
+    players_amount = kivy.properties.NumericProperty(0)
+
     def add_spy(self):
         if(self.spy_amount == 4):
             self.spy_amount = 0
@@ -29,37 +41,72 @@ class ThirdScreen(Screen):
             self.spy_amount = 1
         self.spy_amount -= 1
         print(self.spy_amount)
+
+    def my_value(self, value):
+        self.players_amount = value
+
+    def play(self):
+        client.createLobby(self.players_amount, self.spy_amount)
     pass
 
 class FourthScreen(Screen):
-    token_code = StringProperty('')
+    token_code = kivy.properties.StringProperty('')
+
     def print_token(self, tokdef = ''):
         self.token_code = tokdef
         if(self.token_code != ''):
             print(self.get_token())
+
     def get_token(self):
         return self.token_code
+        
+    pass
+
+class FifthScreen(Screen):
+
+    def rolePopup(self): 
+          
+        layout = GridLayout(cols = 1, padding = 10) 
+        popupLabel = Label(text = "Role: ") 
+        popupLabel2 = Label(text = "Location: ") 
+  
+        layout.add_widget(popupLabel) 
+        layout.add_widget(popupLabel2)       
+  
+        popup = Popup(title ='Role', 
+                      content = layout, 
+                      size_hint =(None, None), size =(200, 200))   
+        popup.open()
     pass
 
 
-class SwitchingScreenApp(App):
-    sm = ObjectProperty(None)
-    
+class RoleScreen(Screen):
+    pass
+
+
+class SwitchingScreenApp(MDApp):
+    screen_manager = kivy.properties.ObjectProperty(None)
+
+    def __init__(self, **kwargs):
+        self.title = "SpyFall"
+        self.theme_cls.theme_style = "Dark"
+        super().__init__(**kwargs)
 
     def build(self):
+        SwitchingScreenApp.screen_manager = ScreenManager()
         
-        SwitchingScreenApp.sm = ScreenManager()
+        welcome_screen = FirstScreen(name="_first_screen_")
+        options_screen = SecondScreen(name="_second_screen_")
+        create_screen = ThirdScreen(name="_third_screen_")
+        connection_screen = FourthScreen(name="_fourth_screen_")
+        playground_screen = FifthScreen(name="_fifth_screen_")
         
-        ws = FirstScreen(name="_first_screen_")
-        os = SecondScreen(name="_second_screen_")
-        crs = ThirdScreen(name="_third_screen_")
-        cos = FourthScreen(name="_fourth_screen_")
-        
-        self.sm.add_widget(ws)
-        self.sm.add_widget(os)
-        self.sm.add_widget(crs)
-        self.sm.add_widget(cos)
-        return self.sm
+        self.screen_manager.add_widget(welcome_screen)
+        self.screen_manager.add_widget(options_screen)
+        self.screen_manager.add_widget(create_screen)
+        self.screen_manager.add_widget(connection_screen)
+        self.screen_manager.add_widget(playground_screen)
+        return self.screen_manager
 
 if __name__ == "__main__":
     
