bc33415b5c776f1614b94cd7e3605b300b16b864
TOIFLMSC <56690053+TOIFLMSC@users.noreply.github.com>
2020-04-28 05:08:28 +0300

    Fixed gachi problems.  No more three hundred bucks.

--- Graphics/Global.py
+++ Graphics/Global.py
@@ -6,6 +6,4 @@ token = ''
 isAdmin = False
 sound1 = SoundLoader.load('Adele - Skyfall.mp3')
 sound2 = SoundLoader.load('Sound Ideas - Metal Light Switch Button Push Off (mp3cut.net).mp3')
-sound3 = SoundLoader.load('Fisting is 300 .mp3')
-sound4 = SoundLoader.load('FUCKYOU.mp3')
-sound5 = SoundLoader.load('Stick your finger in my ass.mp3')
+
